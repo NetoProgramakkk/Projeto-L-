5b757fd1e6d0bfe1c3d37934cab78adf892a476b
NetoProgramakkk <164935764+NetoProgramakkk@users.noreply.github.com>
2024-03-27 15:02:28 -0300

    Update acesso.js

--- acesso.js
+++ acesso.js
@@ -21,13 +21,13 @@ function send() {
         sucesso.style.display = 'inline';
 
         setTimeout(() => {
-            location.href = "/home.html";
+            location.href = "./home.html";
         }, 500);
     } else {
         negado.style.display = 'inline';
 
         setTimeout(() => {
-            location.href = "/erro.html";
+            location.href = "./erro.html";
         }, 500);
     }
 }
